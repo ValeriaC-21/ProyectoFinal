471075177206b5bedb3a0fb4ff99af26aed09970
Valeria Yuliett Caceres Urrutia <valeria.caceres.urrutia@udla.edu.ec>
2026-07-06 07:51:40 -0500

    Delete com/gestion directory

--- com/gestion/Exception/UsuarioDuplicadoException.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.Exception;
-
-//Excepción para evitar el registro de usuarios duplicados en el sistema.
-
-public class UsuarioDuplicadoException extends Exception {
-    public UsuarioDuplicadoException(String mensaje) {
-        super(mensaje);
-    }
-}
